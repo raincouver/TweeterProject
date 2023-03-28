b4895fd269c0776281fb0edaf05fa3008e85642c
Rain Zhu <rain1995zhu@gmail.com>
2023-03-28 16:18:40 -0700

    Update the character counter; change color depends on character length

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,4 +1,14 @@
-$(document).ready(function() {
-  // --- our code goes here ---
-  console.log('ready');
+$(document).ready(function(){
+  $("#tweet-text").on("input", function(){
+      // Print remaining length in counter
+      $("#tweetCounter").text(140 - $(this).val().length);
+      // Change color to red if input is longger than 140
+      if ($(this).val().length > 140) {
+        $("#tweetCounter").css({'color':'red'});
+      }
+      // Change color back to normal if input is below 140 again
+      if ($(this).val().length <= 140) {
+        $("#tweetCounter").css({'color':'#545149'});
+      }
+  });
 });
