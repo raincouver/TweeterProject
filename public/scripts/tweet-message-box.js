800afebfe0ced30c7f307ca82583ba60f17f780a
Rain Zhu <rain1995zhu@gmail.com>
2023-03-28 23:46:43 -0700

    add tweet message box and mouse over animation css

---
+++ public/scripts/tweet-message-box.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  $(".tweetIcon").on("mouseover", function(){
+      // Change color mouseover
+      $(this).css({'color':'#F1C40F'});
+  });
+  $(".tweetIcon").on("mouseout", function(){
+    // Change color back if mouseout
+    $(this).css({'color':'#4056A1'});
+});
+});
+
+$(document).ready(function(){
+  $(".tweet-outline").on("mouseover", function(){
+      // Change color mouseover
+      $(this).css({'box-shadow':'10px 10px #AFAFAF'});
+  });
+  $(".tweet-outline").on("mouseout", function(){
+    // Change color back if mouseout
+    $(this).css({'box-shadow':'none'});
+});
+});
