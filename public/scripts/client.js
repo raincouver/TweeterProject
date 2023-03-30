404f2a989b2c81fe480d208f7e4e46b7597226c0
Rain Zhu <rain1995zhu@gmail.com>
2023-03-29 22:10:13 -0700

    add form and event handler to prevent default submit behaviour

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,17 +7,17 @@
 
 
 $(document).ready(() => {
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
+  const tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
       "handle": "@SirIsaac"
     },
-  "content": {
+    "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-  "created_at": 1461116232227
-}
+    "created_at": 1461116232227
+  };
   const createTweetElement = function(tweetData) {
     // $(() => {
 
@@ -64,31 +64,79 @@ const tweetData = {
   };
 
   const renderTweets = function(tweets) {
-      //loop through tweets
-      for (const tweet of tweets) {
-        //calls createTweetElements for each tweet
-        const $tweet = createTweetElement(tweet);
-        //take returns and appends it to the tweets container
-        $tweetCanvas.prepend($tweet);
-      }
+    //loop through tweets
+    for (const tweet of tweets) {
+      //calls createTweetElements for each tweet
+      const $tweet = createTweetElement(tweet);
+      //take returns and appends it to the tweets container
+      $tweetCanvas.prepend($tweet);
+    }
   };
 
-  const $tweetCanvas = $('.tweetCanvas')
+  //grab the tweet canvas section in the DOM
+  const $tweetCanvas = $('.tweetCanvas');
+
+  $.ajax({
+    method: 'GET',
+    url: '/tweets',
+  }).then((tweets) => {
+    renderTweets(tweets);
+  });
+
+
+  //grab the form from the DOM
+  const $form = $('form');
+
+  //aadd a submit event handler to the form
+  $form.on('submit', (event) => {
+    event.preventDefault();
+    console.log('the form has submitted');
+
+    const urlencoded = $form.serialize();
+    console.log(urlencoded);
+
 
     $.ajax({
-      method: 'GET',
+      method: 'POST',
       url: '/tweets',
-    }).then((tweets) => {
-      renderTweets(tweets);
+      data: urlencoded
+    }).then((newTweet) => {
+      console.log(newTweet);
     });
 
+  });
 });
+
 
 // const $tweet = createTweetElement(tweetData);
 
 // $tweetCanvas.prepend($tweet);
 
 
+const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
 
 
 
